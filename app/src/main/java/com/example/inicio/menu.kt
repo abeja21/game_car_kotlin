ace056dd10d9bc1c3a1179b9c6c8528b13be5489
angel <angel@angel.com>
2021-11-26 13:48:19 +0100

    se vienen cositas

--- app/src/main/java/com/example/inicio/menu.kt
+++ app/src/main/java/com/example/inicio/menu.kt
@@ -1,11 +1,23 @@
 package com.example.inicio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class menu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        var volver= findViewById<Button>(R.id.return1)
+        var ver_coches=findViewById<Button>(R.id.ver_coches)
+        var jugar=findViewById<Button>(R.id.jugar)
+
+        volver.setOnClickListener {
+            val cambiando= Intent(this,MainActivity::class.java)
+            startActivity(cambiando)
+        }
+
+
     }
 }
