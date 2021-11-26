d6da20136d8255307f2e54edb5aa99ef8756214a
angel <angel@angel.com>
2021-11-26 13:12:15 +0100

    Initial commit

---
+++ app/src/main/java/com/example/inicio/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.inicio
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var inicio = findViewById<Button>(R.id.start);
+
+
+        inicio.setOnClickListener {
+            val cambiando= Intent(this,menu::class.java)
+            startActivity(cambiando)
+        }
+    }
+}
