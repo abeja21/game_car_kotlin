309f1c00462a31ae0e0cf37aac3c6c6ff9eff52b
Angel_21 <74176323+abeja21@users.noreply.github.com>
2021-12-01 18:18:40 +0100

    se vienen cositas 14

--- app/src/main/java/com/example/inicio/Cars.kt
+++ app/src/main/java/com/example/inicio/Cars.kt
@@ -1,9 +1,41 @@
 package com.example.inicio
 
 
+
 class Cars (val nombre:String, val velocidadMaxima:Int,val aceleracion:Int,val desaceleracion:Int,val gasolina:Int,val design:Int,val fondo:Int) {
 
+    var velin: Int = 0
+    var fuel = 0;
+    val dr = 0;
 
+    fun acelerate() {
 
+        this.velin += this.aceleracion
+        if (this.velin > this.velocidadMaxima) {
+            this.velin = this.velocidadMaxima
+        } else {
+            this.velin;
+        }
+        this.combustion()
+    }
+    fun frenar(){
 
+        if (this.velin<20){
+            acelerate()
+        }else{
+            this.velin-=this.desaceleracion
+            this.combustion()
+        }
+    }
+    fun combustion(){
+        this.fuel=this.velin/100
+    }
+    fun repostar(){
+        this.fuel+=10;
+        if (this.fuel>this.gasolina){
+            this.fuel=this.gasolina
+        }else{
+            this.fuel
+        }
+    }
 }
--- app/src/main/java/com/example/inicio/juego.kt
+++ app/src/main/java/com/example/inicio/juego.kt
@@ -8,4 +8,5 @@ class juego : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_juego)
     }
+
 }
